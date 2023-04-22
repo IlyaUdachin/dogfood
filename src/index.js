e64d4f22f489fa2828150d3a3ee8ea767b2a8aea
IlyaUdachin <ilya.udachin@gmail.com>
2023-04-22 15:48:59 +0300

    commit_1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(
+//   React.createElement("h1", null, "Hello React!")
+// )
+
+// root.render(
+//   React.createElement(
+//     "div",
+//     {title: "Doggy"},
+//     React.createElement("h1", null, "Hey!"),
+//     React.createElement("p", null, "It is Magic!")
+//   )
+// )
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <div title="Doggy">
+    <h1>Hello</h1>
+    <hr/> 
+    <h2>
+      <mark>React</mark>
+    </h2>
+  </div>
+)
